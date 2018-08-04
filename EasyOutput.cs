e7a52682a0cefb04dcab28369ecca20c42822564
asdf <asdf@example.com>
2018-08-04 17:29:23 +0900

    write EasyOutput

---
+++ EasyOutput.cs
@@ -0,0 +1,46 @@
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace Syy {
+    public static class EasyOutput
+    {
+        private static StringBuilder _stringBuilder = new StringBuilder();
+        public static void Append(string str)
+        {
+            _stringBuilder.Append(str);
+        }
+
+        public static void AppendLine(string str)
+        {
+            _stringBuilder.AppendLine(str);
+        }
+
+        public static void AppendFormat(string format, params object[] args)
+        {
+            _stringBuilder.AppendFormat(format, args);
+        }
+
+        public static void AppendFormatLine(string format, params object[] args)
+        {
+            _stringBuilder.AppendFormat(format, args);
+            _stringBuilder.AppendLine("");
+        }
+
+        public static void Clear()
+        {
+            _stringBuilder = new StringBuilder();
+        }
+
+        public static void Output(string fileNameWithExtention = "output.txt")
+        {
+            string output = _stringBuilder.ToString();
+            StreamWriter writer = new StreamWriter("Assets/" + fileNameWithExtention, false);
+            writer.Write(output);
+            writer.Close();
+
+            Debug.Log(output);
+        }
+    }
+
+}
---
+++ Sample_EasyOutput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Syy.Sample {
+
+    public class Sample_EasyOutput : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            EasyOutput.Clear();
+            EasyOutput.Append("append1");
+            EasyOutput.Append("append2");
+            EasyOutput.Append("append3");
+            EasyOutput.AppendLine("appendLine1");
+            EasyOutput.AppendLine("appendLine2");
+            EasyOutput.AppendLine("appendLine3");
+            EasyOutput.AppendFormat("message {0}","AppendFormat1");
+            EasyOutput.AppendFormat("message {0}", "AppendFormat2");
+            EasyOutput.AppendFormat("message {0}", "AppendFormat3");
+            EasyOutput.AppendFormatLine("message {0}", "AppendFormatLine1");
+            EasyOutput.AppendFormatLine("message {0}", "AppendFormatLine2");
+            EasyOutput.AppendFormatLine("message {0}", "AppendFormatLine3");
+            EasyOutput.Output();
+        }
+    }
+
+}
